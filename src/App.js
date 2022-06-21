8b90ae05f3d3c6582f049ea0e5e266eddae39f0e
xWesleyMartins <wesley_869@hotmail.com.br>
2022-06-21 16:05:21 -0300

    concertando problemas com lint, e implementando requisito 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
-      );
+      <div>
+        <Header />
+        <SolarSystem />
+      </div>
+    );
   }
 }
 
 export default App;
 
-//VQV!
+// VQV!
