98d60aae3be88678d1dfcd2180fc0019e3218841
xWesleyMartins <wesley_869@hotmail.com.br>
2022-06-21 17:07:07 -0300

    completando requisito 3 e 4

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,9 @@
-import React from "react";
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
     const { headline } = this.props;
-
     return (
       <h2>{ headline }</h2>
 
